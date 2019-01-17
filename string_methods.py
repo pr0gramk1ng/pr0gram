d5bd68abe2f6577dd999f3295583761d36c04486
pr0gramk1ng <46227834+pr0gramk1ng@users.noreply.github.com>
2019-01-17 13:33:35 -0500

    Create string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,3 @@
+## Useful Python Documentation: https://docs.python.org/release/2.5.2/lib/string-methods.html
+## https://data36.com/python-built-in-functions-methods-python-data-science-basics-3/
+## Practiacl Data manipulation - https://www.hackerearth.com/practice/machine-learning/data-manipulation-visualisation-r-python/tutorial-data-manipulation-numpy-pandas-python/tutorial/
